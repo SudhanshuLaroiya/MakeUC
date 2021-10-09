8808512172c65d43a3179f0ebe2c4d4e8f25431f
Fayaz Rafin <67512015+fayaz-rafin@users.noreply.github.com>
2021-10-09 16:59:46 -0400

    Update Test.py

--- Test.py
+++ Test.py
@@ -5,6 +5,10 @@ print("chocolate")
 print("chocolate")
 print("its annoying")
 print("cocoa")
+<<<<<<< HEAD
 print("what is the pull??")
 print("Your Mom")
 print("ass")
+=======
+print("what is the pull??")
+>>>>>>> parent of d0ce296... added your mom
